4a524a6249d86ddd5f43aab2d80dcdb3aeb7551f
jyotsnasahu16@gmail.com <tiding792@gmail.com>
2025-06-16 21:26:08 +0530

    multiline poem

---
+++ Python_Intro_Setup/multiline_poem.py
@@ -0,0 +1,14 @@
+poem = """The sun descends, a fiery kiss,
+Upon the world, a gentle bliss.
+
+The stars emerge, a silver grace,
+Each one a smile in time and space.
+
+The moon ascends, a pale white gleam,
+Reflecting light in a silent stream.
+
+And as the world begins to sleep,
+A peaceful hush the shadows keep. 
+"""
+
+print(poem)
