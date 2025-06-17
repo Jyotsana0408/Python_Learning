ba1c2679e0052deb5e755fca820688a130049ae5
jyotsnasahu16@gmail.com <tiding792@gmail.com>
2025-06-17 18:38:46 +0530

    mini project 1 - Self Intro

---
+++ Python_Intro_Setup/mini_project_1/biodata.py
@@ -0,0 +1,5 @@
+name = "Jyotsna"
+hoobies = ["playing chess", "listening to music"]
+goal = "to become best python programmer"
+
+print(f"Hi everyone , My name is {name}. My hobbies are {hoobies[0]} and {hoobies[1]}. My goal is {goal}")
