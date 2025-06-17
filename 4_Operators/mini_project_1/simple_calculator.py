b4c336fb0404bfde872cec85697c47c11f29bf94
jyotsnasahu16@gmail.com <tiding792@gmail.com>
2025-06-17 19:42:23 +0530

    operators with mini project

---
+++ 4_Operators/mini_project_1/simple_calculator.py
@@ -0,0 +1,21 @@
+"""
+Perform simple math add, subtract , multiplication and divide with two variables.
+"""
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+# add two numbers
+print(a+b)
+# subtract two numbers
+print(a-b)
+# multiply two numbers
+print(a*b)
+# divide two numbers
+print(a/b)
+# remainder of two numbers
+print(a%b)
+# power
+print(a**2)
+# grater than
+print(a>b)
