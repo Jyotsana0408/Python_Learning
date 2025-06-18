bd90e90a8a1be309e595b4afba3e57dffce4891f
jyotsnasahu16@gmail.com <tiding792@gmail.com>
2025-06-18 22:17:12 +0530

    motivational quotes mini project

---
+++ 6_Revision_1/motivationl_quotes.py
@@ -0,0 +1,9 @@
+import random
+
+thought_of_the_day = ["You have within you, right now, everything you need to deal with whatever the world can throw at you - Brian Tracy",
+                      "The only impossible journey is the one you never begin — Tony Robbins",
+                      "Success is not final, failure is not fatal: It is the courage to continue that counts - Winston Churchill",
+                      "Rise above the storm and you will find the sunshine — Mario Fernández",
+                      "Nothing is impossible, the word itself says I am possible - Audrey Hepburn"]
+
+print(random.choice(thought_of_the_day))
