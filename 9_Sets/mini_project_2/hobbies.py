595194818b741b0c80c681b45ee670b585b06ed7
jyotsnasahu16@gmail.com <tiding792@gmail.com>
2025-06-24 15:12:29 +0530

    hobbies with set mini project 2

---
+++ 9_Sets/mini_project_2/hobbies.py
@@ -0,0 +1,8 @@
+hobbies_person1 = {"reading", "watching movies", "playing chess", "travelling"}
+hobbies_person2 = {"reading", "cooking", "volunterring"}
+
+res = hobbies_person1.difference(hobbies_person2)
+print(res)
+
+res1 = hobbies_person2.difference(hobbies_person1)
+print(res1)
