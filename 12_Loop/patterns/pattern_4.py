9e1fb59f95e7e5c9b866a3cb82b8f5628a824fdb
jyotsnasahu16@gmail.com <tiding792@gmail.com>
2025-06-30 20:11:51 +0530

    added pattern

---
+++ 12_Loop/patterns/pattern_4.py
@@ -0,0 +1,11 @@
+# Right-Aligned Pattern
+
+row = 5
+
+for i in range(1, row + 1):
+    print(" " * (row - i) + "*" * i)
+
+
+# rows = 5
+# for i in range(1, rows + 1):
+#     print(" " * (rows - i) + "*" * i)
---
+++ 12_Loop/patterns/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+ = 5
+
+for i in range(1, row+1):
+    print(" " * (r
